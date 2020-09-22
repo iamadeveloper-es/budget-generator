ae6c9007fbe37ddb19a30a444b356b25aa0915cd
DivthesignDevLab <daniel.verdu@e.craftww.com>
2020-09-22 16:50:10 +0200

    logica de la creación de la factura y validación del formulario

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,110 @@
-let c, dateInpt, companyNameInpt, cIFInpt, addressInpt, conceptInpt, baseInpt, iRPFInpt, iVAInpt, budgetNodo, createBudgetBtn, budgetObj, budgetsArray, dateOutput, companyNameOutput, cIFOutput, addressOutput, conceptOutput, baseOutput, iRPFOutput, iVAOutput, totalOutput
-let percent, eur;
-c = console.log;
-companyNameInpt = document.querySelector('#company-name');
-dateInpt = document.querySelector('#date');
-cIFInpt = document.querySelector('#cif');
-addressInpt = document.querySelector('#adress');
-conceptInpt = document.querySelector('#concept');
-baseInpt = document.querySelector('#base');
-iRPFInpt = document.querySelector('#irpf');
-iVAInpt = document.querySelector('#iva');
-createBudgetBtn = document.querySelector('#crear');
-dateOutput = document.querySelector('.budget__fecha');
-companyNameOutput = document.querySelector('.budget__company-name');
-cIFOutput = document.querySelector('.budget__cif');
-addressOutput = document.querySelector('.budget__address');
-conceptOutput = document.querySelector('.budget__concept');
-baseOutput = document.querySelector('.budget__base');
-iRPFOutput = document.querySelector('.budget__irpf');
-iVAOutput = document.querySelector('.budget__iva');
-totalOutput = document.querySelector('.budget__total');
-budgetNodo  = document.querySelector('#budget');
-budgetsArray = [];
-percent = '%'
-eur = '€'
+const c = console.log;
+const companyNameInpt = document.querySelector('#company-name');
+const textInpt = document.querySelectorAll('input[type=text]');
+const dateInpt = document.querySelector('#date');
+const checkInpt = document.querySelector('#today-check');
+const cIFInpt = document.querySelector('#cif');
+const addressInpt = document.querySelector('#adress');
+const conceptInpt = document.querySelector('#concept');
+const baseInpt = document.querySelector('#base');
+const iRPFInpt = document.querySelector('#irpf');
+const iVAInpt = document.querySelector('#iva');
+const errorMssg = document.querySelectorAll('.error-mssg');
+const createBudgetBtn = document.querySelector('#crear');
+const dateOutput = document.querySelector('.budget__fecha');
+const companyNameOutput = document.querySelector('.budget__company-name');
+const cIFOutput = document.querySelector('.budget__cif');
+const addressOutput = document.querySelector('.budget__address');
+const conceptOutput = document.querySelector('.budget__concept');
+const baseOutput = document.querySelector('.budget__base');
+const iRPFOutput = document.querySelector('.budget__irpf');
+const iVAOutput = document.querySelector('.budget__iva');
+const totalOutput = document.querySelector('.budget__total');
+const budgetNodo  = document.querySelector('#budget');
+let budgetsArray = [];
+let budgetObj;
+const percent = '%'
+const eur = '€'
 
-createBudgetBtn.addEventListener("click", submit);
+//Handlers
+createBudgetBtn.addEventListener('click', validateForm);
+checkInpt.addEventListener('click', changeDate)
+
+
+function changeDate(){
+    checkInpt.checked != checkInpt.checked;
+    if(checkInpt.checked){
+        printDate(getDate())
+    }else{
+        dateInpt.value = ''
+    }
+};
+
+const setMonth = function(month){
+    if(month < 10){
+        return '0'+month;
+    }
+};
+const getDate = function(){
+    const getFecha = new Date();
+    const getDay = getFecha.getDate();
+    const getMonth =  getFecha.getMonth();
+    const getYear = getFecha.getFullYear();
+    const fecha = `${getDay}/${setMonth(getMonth)}/${getYear}`;
+    return fecha;
+};
+const printDate = function(date){
+    dateInpt.value = date
+}
 
 
 
-function getInptValue(inpt){
+function validateForm(e){
+    e.preventDefault();
+    let error = [];
+    if(dateInpt.value == ''){
+        error.push('La fechas está vacía');
+    }
+    if(cIFInpt.value == ''){
+        error.push('El CIF está vacío');
+    }
+    if(companyNameInpt.value == ''){
+        error.push('El Nombre de la Compañía está vacio');
+    }
+    if(addressInpt.value == ''){
+        error.push('La dirección está vacía');
+    }
+    if(baseInpt.value == ''){
+        error.push('La Base Imponible está vacía');
+    }
+    if(iRPFInpt.value == ''){
+        error.push('El IRPF está vacío');
+    }
+    if(iVAInpt.value == ''){
+        error.push('El IVA está vacío');
+    }
+    if(error.length == 0){
+        submit(e)
+    }
+    c(error)
+};
+
+const getInptValue = function(inpt){
     return inpt.value
 };
 
-function generateBudget(){
+const cleanInptValue = function(){
+    companyNameInpt.value = '';
+    cIFInpt.value = '';
+    addressInpt.value = '';
+    conceptInpt.value = '';
+    baseInpt.value = '';
+    iRPFInpt.value = '';
+    iVAInpt.value = '';
+
+}
+
+const generateBudget = function(){
     budgetObj = {
         date: getDate(),
         CompanyName: getInptValue(companyNameInpt),
@@ -44,18 +116,18 @@ function generateBudget(){
         iva: Number(getInptValue(iVAInpt)),
         total: ''
     };
-    budgetsArray.push(budgetObj);
-    c(budgetsArray);
+    cleanInptValue();
+    return budgetsArray.push(budgetObj);
 }
 
-function calcTotal(base){
-    let totalIva = (base * 21) / 100;
-    let totalIRPF = (base * 7) / 100;
-    let total = (base + totalIva) - totalIRPF
+const calcTotal = function(base){
+    const totalIva = (base * 21) / 100;
+    const totalIRPF = (base * 7) / 100;
+    const total = (base + totalIva) - totalIRPF
     return total;
 };
 
-function printBudget(budget){
+const printBudget = function(budget){
     budget = JSON.stringify(budget);
     dateOutput.innerHTML = budgetObj.date;
     companyNameOutput.innerHTML = budgetObj.CompanyName;
@@ -69,12 +141,17 @@ function printBudget(budget){
     
 
 }
+const showBudgetSection = function(){
+    budgetNodo.classList.add('section__budget');
+    budgetNodo.classList.remove('hide');
+};
 
 
 function submit(evt){
     evt.preventDefault();
-    budgetNodo.classList.add('section__budget');
-    budgetNodo.classList.remove('hide');
-    generateBudget()
-    printBudget(budgetObj)
+    c('Budget creado');
+    generateBudget();
+    printBudget(budgetObj);
+    showBudgetSection();
+    //window.print();
 }
